98e37e47ebab9ab1cf8455e50813d3caebf9aec6
Farid Machrou <fmachrou@gmail.com>
2024-04-30 13:14:12 +0200

    job finished

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,40 +1,42 @@
 # -*- coding: utf-8 -*-
 
+# dictionnary to define age/quality update rule per item
+age_quality = {
+    'Aged Brie' : {100:'+1',0:'+2'},
+    'Backstage passes' : {100:'+1',10:'+2',5:'+3',0:'*0'},
+    'Conjured': {100:'-2',0:'-4'},
+    # legendary item, defined integer value
+    'Sulfuras' : {100:80,0:80},
+    # default value
+    '':{100:'-1',0:'-2'},
+    # new item to add here
+    #'Item.name' : { nb_days : quality_update_rule, ... },
+    }
+
+# max quality per item
+min_quality = 0
+# min quality per item
+max_quality = 50
+
 class GildedRose(object):
 
     def __init__(self, items):
         self.items = items
 
     def update_quality(self):
+        # for each item do
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
+            # get item index in dictionnary
+            index = [x for x in age_quality.keys() if x in item.name][0]
+            # get related rule
+            rule = age_quality[index][[dl for dl in age_quality[index] if item.sell_in <= dl][-1]]
+            # if the rule is not an integer (not legendary item)
+            if type(rule) == str:
                 item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
-
+                item.quality = min(max(eval(str(item.quality) + rule), min_quality), max_quality)
+            # else the rule is an integer (a legendary item)
+            else :
+                item.quality = rule
 
 class Item:
     def __init__(self, name, sell_in, quality):
